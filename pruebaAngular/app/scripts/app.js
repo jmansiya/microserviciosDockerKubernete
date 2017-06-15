8257e7db68614289f576c937fafbd266490dc6cd
Jose Mansilla <jmansiya@users.noreply.github.com>
2017-06-16 00:55:07 +0200

    subida inicial proyecto angularjs
    Subida inicial proyecto angular que consume los microservicios del repositorio.

---
+++ pruebaAngular/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pruebaAngularApp
+ * @description
+ * # pruebaAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pruebaAngularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ pruebaAngular/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pruebaAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pruebaAngularApp
+ */
+angular.module('pruebaAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
